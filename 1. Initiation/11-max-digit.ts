2d5b3cd8076591371162f4a10f62ef41d724a9e2
Paul den Boer <paul@levarne.nl>
2022-03-14 08:46:14 +0100

    11

---
+++ 1. Initiation/11-max-digit.ts
@@ -0,0 +1,15 @@
+import assert from "assert";
+
+var maxDigit = (value: number): number => Math.max(...value.toString().split('').map(e => parseInt(e)));
+
+console.log('Example:');
+console.log(maxDigit(0));
+
+// These "asserts" are used for self-checking
+assert.equal(maxDigit(0), 0);
+assert.equal(maxDigit(52), 5);
+assert.equal(maxDigit(634), 6);
+assert.equal(maxDigit(1), 1);
+assert.equal(maxDigit(10000), 1);
+
+console.log("Coding complete? Click 'Check' to earn cool rewards!");
