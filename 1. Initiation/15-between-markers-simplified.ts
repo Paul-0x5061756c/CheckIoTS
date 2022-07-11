36ef13e882f7d18bd7ef844cceea7d8f65f3954a
Paul den Boer <paul@levarne.nl>
2022-07-11 16:50:34 +0200

    testing the github action

--- 1. Initiation/15-between-markers-simplified.ts
+++ 1. Initiation/15-between-markers-simplified.ts
@@ -3,7 +3,7 @@ import assert from "assert";
 var betweenMarkers = (line: string, left: string, right: string) : string => line.split(left)[1].split(right)[0];
 console.log('Example:');
 console.log(betweenMarkers('What is >apple<', '>', '<'));
-
+  
 // These "asserts" are used for self-checking
 assert.equal(betweenMarkers('What is >apple<', '>', '<'), 'apple');
 assert.equal(betweenMarkers('What is [apple]', '[', ']'), 'apple');
